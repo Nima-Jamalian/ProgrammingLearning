aa56ab006d8c9064906498a8bac136c912322f56
Nima Jamalian <nima.jamalian80@gmail.com>
2022-04-11 20:37:12 +0100

    Lesson 1

---
+++ Lessons/dataTypes.py
@@ -0,0 +1,7 @@
+print("Data Types:")
+#Using type function to get the data type of different objects
+print(type(1))
+print(type(2.0))
+print(type("Hello World"))
+print(type(True))
+print(type(None))
